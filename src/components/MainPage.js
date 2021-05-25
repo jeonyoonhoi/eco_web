e1d65662711f1f5e6ad5a6b0c59a4ebc3107a9d0
Yoon hoi, Jeon <une0927@naver.com>
2021-05-26 06:06:35 +0900

    Update MainPage.js

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -20,8 +20,8 @@ class MainPage extends Component{
         return (
             <div id="MainPageId">
                 <h1>안녕하세요?</h1>
-                <h3> 저는 재활용 방법 알리미 챗봇입니다. </br>
-            생활 속 재활용 방법에 대한 정보를 알려드리기 위해 만들어졌어요! </br>
+                <h3> 저는 재활용 방법 알리미 챗봇입니다. <br/>
+            생활 속 재활용 방법에 대한 정보를 알려드리기 위해 만들어졌어요! <br/>
             시작해볼까요? </h2>
                 <div className="form-group">
                     <button onClick={this.moveLoginPage} className="btn btn-primary mt-2" type="submit">
