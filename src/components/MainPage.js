a2f283754c6265aa1b68aa3eecb221e70084bbdc
Yoon hoi, Jeon <une0927@naver.com>
2021-05-26 06:17:43 +0900

    Update MainPage.js

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -22,7 +22,7 @@ class MainPage extends Component{
                 <h1>안녕하세요?</h1>
                 <h3> 저는 재활용 방법 알리미 챗봇입니다. <br/>
             생활 속 재활용 방법에 대한 정보를 알려드리기 위해 만들어졌어요! <br/>
-            시작해볼까요? </h2>
+            시작해볼까요? </h3>
                 <div className="form-group">
                     <button onClick={this.moveLoginPage} className="btn btn-primary mt-2" type="submit">
                         시작하기
